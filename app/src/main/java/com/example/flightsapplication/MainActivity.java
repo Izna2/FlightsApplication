4f53e8bd3238680372487c851e3eb56fe2158c00
Izna2 <88369245+Izna2@users.noreply.github.com>
2022-02-07 19:34:21 +0500

    Fragments and Tabs added

--- app/src/main/java/com/example/flightsapplication/MainActivity.java
+++ app/src/main/java/com/example/flightsapplication/MainActivity.java
@@ -1,14 +1,57 @@
 package com.example.flightsapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.viewpager.widget.ViewPager;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
 
+import com.google.android.material.tabs.TabItem;
+import com.google.android.material.tabs.TabLayout;
+
 public class MainActivity extends AppCompatActivity {
 
+    TabLayout tl;
+    TabItem Flight_number,Routes;
+    ViewPager2 viewpager;
+    FragmentAdapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tl = findViewById(R.id.tablayout);
+        Flight_number = findViewById(R.id.fnumber);
+        Routes = findViewById(R.id.routes);
+        viewpager = findViewById(R.id.vpager);
+
+        FragmentManager fm = getSupportFragmentManager();
+        adapter = new FragmentAdapter(fm, getLifecycle());
+        viewpager.setAdapter(adapter);
+
+        tl.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewpager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+
+        });
+        viewpager.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                tl.selectTab(tl.getTabAt(position));
+            }
+        });
     }
 }
